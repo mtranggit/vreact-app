f7c0a459140a6bb473c2f961f6408576c538124b
Michael Trang <michael.trang@hotmail.com>
2024-09-07 11:28:32 +0000

    update test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,10 +1,20 @@
 import App from '@/App';
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 
 describe('App', () => {
-  it('should render correctly', () => {
+  it('should render correctly', async () => {
     render(<App />);
     const heading = screen.getByRole('heading');
     expect(heading).toHaveTextContent(/vite \+ react/i);
+
+    const user = userEvent.setup();
+    const button = screen.getByRole('button', { name: /count is/i });
+    expect(button).toHaveTextContent(/count is 0/i);
+    await user.click(button);
+    expect(button).toHaveTextContent(/count is 1/i);
+    expect(
+      screen.getByText('Click on the Vite and React logos to learn more')
+    ).toBeInTheDocument();
   });
 });
